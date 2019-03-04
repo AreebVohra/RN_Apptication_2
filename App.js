7a8406449c115b896bfad55da3c3b72c03f0f4d7
AreebVohra <areebvohra@gmail.com>
2019-03-04 23:32:53 +0500

    new navigation

--- App.js
+++ App.js
@@ -1,70 +1,83 @@
 import React, { Component } from 'react';
-import { Text, View, Image, StatusBar, StyleSheet, ImageBackground } from 'react-native';
-import { Container, Header, Left, Title, Right, Body, Button, Icon, Tab, Tabs, StyleProvider } from 'native-base';
-import getTheme from './native-base-theme/components';
-import material from './native-base-theme/variables/material';
+import { Text, View, SafeAreaView, ImageBackground, StatusBar } from 'react-native';
+import { Container, Header, Left, Title, Right, Body, Button, Icon } from 'native-base';
+
+
+import {
+  createAppContainer,
+  createMaterialTopTabNavigator,
+} from 'react-navigation';
+
 import HomeScreen from './tabs/Home';
 import ProductScreen from './tabs/Product';
 import HistoryScreen from './tabs/History';
 import StatisticsScreen from './tabs/Statistics'
 
-
-
 export default class App extends Component {
+  componentDidMount() {
+    StatusBar.setHidden(true);
+  }
   render() {
     return (
-      <ImageBackground source={require('./gradient.jpg')} style={styles.container}>
-        <StatusBar hidden />
-        <StyleProvider style={getTheme(material)}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ImageBackground source={require('./gradient.jpg')} style={{ flex: 1, width: null }}>
           <Container style={{ backgroundColor: 'transparent' }}>
-            <Header style={{ backgroundColor: 'rgba(52, 52, 52, 0.4)', height: 60 }} >
-              <Left>
-                <Button transparent>
-                  <Icon name='arrow-back' />
-                </Button>
-              </Left>
+            <Header noLeft noShadow
+              indicatorStyle={{ backgroundColor: 'white', height: 3, }} 
+              style={{ backgroundColor: 'rgba(52, 52, 52, 0.4)', height: 60 }}>
+              <Left></Left>
               <Body>
                 <Title>Header</Title>
               </Body>
               <Right>
                 <Button transparent>
-                  <Icon name='menu' />
+                  <Icon name='settings' />
                 </Button>
               </Right>
             </Header>
-            <Tabs tabContainerStyle={{ height: 60 }}>
-              <Tab style={{ backgroundColor: 'transparent' }} heading="Home">
-                <HomeScreen />
-              </Tab>
-              <Tab heading="Products">
-                <ProductScreen />
-              </Tab>
-              <Tab heading="History">
-                <HistoryScreen />
-              </Tab>
-              <Tab heading="Statistics">
-                <StatisticsScreen />
-              </Tab>
-            </Tabs>
+            <AppContainer />
           </Container>
-        </StyleProvider>
-      </ImageBackground>
+        </ImageBackground>
+      </SafeAreaView>
     );
   }
 }
 
+const TabNavigator = createMaterialTopTabNavigator({
+  Home: HomeScreen,
+  Products: ProductScreen,
+  History: HistoryScreen,
+  Statistics: StatisticsScreen,
+}, {
+    defaultNavigationOptions: {
+      headerStyle: {
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: 'center',
-    width: null,
-    height: null,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+      }
+    },
+    tabBarOptions: {
+      activeTintColor: '#f2f2f2',
+      activeBackgroundColor: '#2EC4B6',
+      labelStyle: {
+        fontSize: 13,
+        fontWeight: 'bold',
+        margin: 0,
+        padding: 0
+      },
+
+      tabStyle: {
+        height: 50,
+
+      },
+      indicatorStyle: {
+        backgroundColor: 'white',
+        height: 3,
+      },
+      style: {
+        backgroundColor: 'rgba(52, 52, 52, 0.4)',
+        elevation: 0,
+      },
+      initialRouteName: 'Home',
+    }
+  });
+
+const AppContainer = createAppContainer(TabNavigator);
---
+++ App_old.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Text, View, Image, StatusBar, StyleSheet, ImageBackground } from 'react-native';
+import { Container, Header, Left, Title, Right, Body, Button, Icon, Tab, Tabs, StyleProvider } from 'native-base';
+import getTheme from './native-base-theme/components';
+import material from './native-base-theme/variables/material';
+import HomeScreen from './tabs/Home';
+import ProductScreen from './tabs/Product';
+import HistoryScreen from './tabs/History';
+import StatisticsScreen from './tabs/Statistics'
+
+export default class App extends Component {
+  render() {
+    return (
+      <ImageBackground source={require('./gradient.jpg')} style={styles.container}>
+        <StatusBar hidden />
+        <StyleProvider style={getTheme(material)}>
+          <Container style={{ backgroundColor: 'transparent' }}>
+            <Header style={{ backgroundColor: 'rgba(52, 52, 52, 0.4)', height: 60 }}>
+              <Left>
+                <Button transparent>
+                  <Icon name='arrow-back' />
+                </Button>
+              </Left>
+              <Body>
+                <Title>Header</Title>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Icon name='menu' />
+                </Button>
+              </Right>
+            </Header>
+            <Tabs tabContainerStyle={{ height: 60 }}>
+              <Tab style={{ backgroundColor: 'transparent' }} heading="Home">
+                <HomeScreen />
+              </Tab>
+              <Tab heading="Products">
+                <ProductScreen />
+              </Tab>
+              <Tab heading="History">
+                <HistoryScreen />
+              </Tab>
+              <Tab heading="Statistics">
+                <StatisticsScreen />
+              </Tab>
+            </Tabs>
+          </Container>
+        </StyleProvider>
+      </ImageBackground>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center',
+    width: null,
+    height: null,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
